bbea706d1dd59b9f399c9c26261c1bed1a534cfd
wayoptimistic <jbehrens57@gmail.com>
2026-03-18 14:21:52 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 
@@ -9,5 +9,5 @@ app.get("/health", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
